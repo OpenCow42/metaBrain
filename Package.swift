c77a2e395cc8680431c2e5203844d6f745a02e77
Adrien <vroomvroom@weavee.net>
2026-05-16 23:12:32 +0200

    chore: add swift-leveldb dependency

--- Package.swift
+++ Package.swift
@@ -25,11 +25,18 @@ let package = Package(
         .package(
             url: "https://github.com/apple/swift-argument-parser.git",
             from: "1.5.0"
+        ),
+        .package(
+            url: "git@github.com:OpenCow42/swift-leveldb.git",
+            branch: "main"
         )
     ],
     targets: [
         .target(
-            name: "MetaBrainCore"
+            name: "MetaBrainCore",
+            dependencies: [
+                .product(name: "swift-leveldb-zstd", package: "swift-leveldb")
+            ]
         ),
         .executableTarget(
             name: "MetaBrainCLI",
